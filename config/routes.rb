b76c6e584ec359194ca257f4be1d46f5792d53dd
bweikulrich <bweikulrich@me.com>
2014-01-06 22:25:28 -0800

    creates a signout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Tagsurf::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  devise_scope :user do
+     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   root 'static#index' 
 
 end
