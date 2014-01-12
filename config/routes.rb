7c1c9ca0e35ed3e89892630c44b27f6f8ffb8acb
bweikulrich <bweikulrich@me.com>
2014-01-12 11:20:02 -0800

    voting single card routes kickoff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Tagsurf::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => 'sessions' }
 
+
+  # Votes
+  post   'votes/:resource/:id'             => 'votes#create'
+  delete 'votes/:resource/:id'             => 'votes#destroy'
+
+  # Cards
+  get    'cards'                           => 'cards#recent'
+  get    'cards/:id'                       => 'cards#show'
+
+  # Users
+  get    'users/:slug/votes'               => 'users#votes'
+  get    'user/:slug/voting'               => 'users#needs_vote'
+
   root 'static#index' 
 
 end
