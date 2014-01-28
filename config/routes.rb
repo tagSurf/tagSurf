80e87cece828e6a29a161950932f59d386273a81
bweikulrich <bweikulrich@me.com>
2014-01-27 21:47:16 -0800

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Tagsurf::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => 'sessions' }
+
+  get '/users/sign_in', to: redirect('/')
+  get '/users/sign_up', to: redirect('/')
  
   # Voting
   get 'votes/:id/:vote'  => 'cards#add_vote'
