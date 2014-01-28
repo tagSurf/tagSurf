8515cee3ddb185ab3d0127a63cb3ce7e3f62287d
bweikulrich <bweikulrich@me.com>
2014-01-27 22:06:27 -0800

    remove devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Tagsurf::Application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => 'sessions' }
-
   get '/users/sign_in', to: redirect('/')
   get '/users/sign_up', to: redirect('/')
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => 'sessions' }
  
   # Voting
   get 'votes/:id/:vote'  => 'cards#add_vote'
