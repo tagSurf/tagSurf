23d8c98d98d5cdeca41d7ca9e12ce806cd768add
bweikulrich <bweikulrich@me.com>
2014-01-27 22:14:58 -0800

    db validation on user save

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
   CLIENT_ID = Rails.env.production? ? 'e0d1a9753eaf289' : '63c3978f06dac10'
   CLIENT_SECRET = Rails.env.production? ? '804e630c072f527b68bdfcc6a08ccbfe2492ab99' : '4eea9bc017f984049cfcd748fb3d8de17ae1cb8e'
 
+
+  validates :beta_user, inclusion: [true]
+
   def expired_imgur_token?
     Time.now > imgur_token_expires_at
   end 
