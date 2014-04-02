18621b23d9f93a4f0efde0743da3c55ef37d11cf
k_fluff <drmartinlutherchang@gmail.com>
2014-04-01 19:28:51 -0700

    added test data

--- front/js/test_data.js
+++ front/js/test_data.js
@@ -1,8 +1,108 @@
 data = 
 {
 	{
-		"src" :
-
+		"src" : "i.imgur.com/TDLKlz8.gif",
+		"tag" : "gifs",
+		"likes" : "37",
+		"dislikes" : "1",
+		"title" : "Just joined Imgur, hi user sub!",
+		"date" : "2014-03-31T20:44:55"
 	},
+	{
+		"src" : "i.imgur.com/6cgxeAa.png",
+		"tag" : "adviceAnimals",
+		"likes" : "300",
+		"dislikes" : "150",
+		"title" : "It's petty, but watching him lose it during class is my entertainment for the day.",
+		"date" : "2014-04-01T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/WuqHnEE.jpg",
+		"tag" : "funny",
+		"likes" : "1116",
+		"dislikes" : "35",
+		"title" : "Spelling matters",
+		"date" : "2014-03-25T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/29vf73V.jpg",
+		"tag" : "gaming",
+		"likes" : "300",
+		"dislikes" : "1932",
+		"title" : "My favorite thing about Goat Simulator so far.",
+		"date" : "2014-03-21T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/SltaKer.jpg",
+		"tag" : "aww",
+		"likes" : "19234",
+		"dislikes" : "374",
+		"title" : "Aww, yes shake my hand",
+		"date" : "2014-03-27T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/sQbxhkM.jpg",
+		"tag" : "earthporn",
+		"likes" : "234",
+		"dislikes" : "17",
+		"title" : "High Shoal Falls - North Carolina",
+		"date" : "2014-03-20T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/j6KHs0B.jpg",
+		"tag" : "funny",
+		"likes" : "721",
+		"dislikes" : "415",
+		"title" : "Live with 2 girls who are afraid of their own shadows, left this in their bathroom for April Fools Day with the door shut.",
+		"date" : "2014-03-01T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/W6z6Lsz.gif",
+		"tag" : "gifs",
+		"likes" : "313",
+		"dislikes" : "29",
+		"title" : "I always thought these were stencils. Impressive.",
+		"date" : "2014-02-27T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/kaUJbjX.gif",
+		"tag" : "AskReddit",
+		"likes" : "19",
+		"dislikes" : "0",
+		"title" : "How many of you reddit at work?",
+		"date" : "2014-02-23T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/4zaCJAW.png",
+		"tag" : "AdviceAnimals",
+		"likes" : "0",
+		"dislikes" : "112",
+		"title" : "Saw some new Facebook drama today",
+		"date" : "2014-02-26T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/YtK3TL7.jpg",
+		"tag" : "AdviceAnimals",
+		"likes" : "257",
+		"dislikes" : "56",
+		"title" : "2 women in my office were talking about the best way to cook bacon. 1 said the oven while the other said microwave. This was my comment.",
+		"date" : "2014-02-17T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/mgIWw0Z.jpg",
+		"tag" : "funny",
+		"likes" : "743",
+		"dislikes" : "803",
+		"title" : "There is a clever Target employee in my town.",
+		"date" : "2014-02-13T20:44:55"
+	},
+	{
+		"src" : "i.imgur.com/KtQoX5v.jpg",
+		"tag" : "funny",
+		"likes" : "967",
+		"dislikes" : "940",
+		"title" : "Long night down at the mine...",
+		"date" : "2014-02-03T20:44:55"
+	}
 	
 }
