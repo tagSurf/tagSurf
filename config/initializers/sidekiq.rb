a9c4ba05bcbe026d5b2186d9abc290fe16b95c2b
bweikulrich <bweikulrich@me.com>
2014-04-04 12:19:25 -0700

    adds DB config to initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,11 @@ redis_url = Rails.env.development? ? 'redis://localhost:6379/0' : ENV['OPENREDIS
 
 Sidekiq.configure_server do |config|
   config.redis = { :url => redis_url, :namespace => 'tagsurf' }
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ActiveRecord::Base.establish_connection
+  end
 end
 
 Sidekiq.configure_client do |config|
