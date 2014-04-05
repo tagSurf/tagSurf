3450d8ecff2f60c23a2d47cbdd8ec3285370ca4c
bweikulrich <bweikulrich@me.com>
2014-04-04 18:09:40 -0700

    removes middleware for redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :get_tags
+  before_filter :get_tags, :redirect_desktops
 
   def get_tags
     @tags = Tag.all
   end
+
+  def redirect_desktops
+    return unless Rails.env.production? && !browser.mobile? 
+    if browser.current_user && current_user.admin? 
+      redirect_to sidekiq_web_path
+    else
+      redirect_to device_path
+    end
+  end 
 
 end
--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,5 +1,6 @@
 Rails.configuration.middleware.use Browser::Middleware do
   if Rails.env.production? && !browser.mobile?
-    redirect_to render_sidekiq_path
+    # Moved to ApplicationController 
+    # redirect_to device_path
   end
 end
