65bc21fb75f06ea2d434f28e8bf6f77fbfee4536
bweikulrich <bweikulrich@me.com>
2014-04-17 16:22:36 -0700

    confirm vote doesnt exist before creating

--- app/workers/create_favorite_vote.rb
+++ app/workers/create_favorite_vote.rb
@@ -4,13 +4,21 @@ class CreateFavoriteVote
   def perform(favorite_id)
     begin
       fav = Favorite.find(favorite_id)
-      vote = Vote.create!(
-        voter_id: fav.user.id,
-        voter_type: 'User',
-        votable_id: fav.card.id,
-        votable_type: 'Card',
-        vote_flag: true
-      )
+      vote = Vote.where(
+        votable_id: fav.card.id, 
+        votable_type: 'Card', 
+        voter_id: fav.user.id, 
+        voter_type: 'User'
+      ).first
+      unless vote
+        Vote.create!(
+          voter_id: fav.user.id,
+          voter_type: 'User',
+          votable_id: fav.card.id,
+          votable_type: 'Card',
+          vote_flag: true
+        )
+      end
     rescue => e
       raise "Unable to create vote after favorite: #{e}"
     end
