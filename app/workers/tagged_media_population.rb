860e8e454ceecb988b00cb8dde1e76ed05f3e032
bweikulrich <bweikulrich@me.com>
2014-04-17 22:12:55 -0700

    sets tagged media worker to run in 25 tag batches every 15mins

--- app/workers/tagged_media_population.rb
+++ app/workers/tagged_media_population.rb
@@ -1,7 +1,18 @@
 class TaggedMediaPopulation
   include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-  def perform(tag)
-    Card.populate_tag(tag)
+  recurrence { hourly.minute_of_hour(5, 20, 35, 50) }
+
+  def perform
+    batch = Tag.where(fetch_more_content: true).limit(25)
+    if batch.present?
+      batch.each do |tag|
+        Card.populate_tag!(tag)
+        tag.update_column("fetch_more_content", false)
+      end
+    else
+      Tag.update_all(fetch_more_content: true)
+    end
   end
 end
