383df2a7a14791a66a319015b54668abd6ac08ba
bweikulrich <bweikulrich@me.com>
2014-04-30 22:35:50 -0700

    access code validations

--- app/models/access_code.rb
+++ app/models/access_code.rb
@@ -1,5 +1,6 @@
 class AccessCode < ActiveRecord::Base
 
+  validates_presence_of :name, :code
 
   before_validation(on: :create) do
     generate_activation_code
@@ -9,7 +10,7 @@ class AccessCode < ActiveRecord::Base
   # ie... B versus 8, 0 versus O
   def generate_activation_code(size = 6)
     charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
-    (0...size).map{ charset.to_a[rand(charset.size)] }.join
+    self.code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
   end
 
 end
