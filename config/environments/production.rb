e5e385a0bbabc58e95bb6ce7e2b44cfc413d1a67
bweikulrich <bweikulrich@me.com>
2014-05-01 20:51:04 -0700

    adds url config to prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,9 @@
 Tagsurf::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  config.action_mailer.default_url_options = { host: 'tagsurf.herokuapp.com' }
+
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
