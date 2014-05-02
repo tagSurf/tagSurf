a3400390db5a07434a2d56e9e7c5a6c4d7e1f5f3
bweikulrich <bweikulrich@me.com>
2014-05-02 11:39:33 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-Tag.create!(name: 'imgurhot')
-Card.populate!
-User.create!(email: 'admin@example.com', password: '12345678', password_confirmation: '12345678', beta_user: true)
+unless Tag.where(name: 'imgurhot')
+  Tag.create(name: 'imgurhot')
+end
+
+Card.populate_trending!
+
+unless User.where(email: 'admin@example')
+  User.create(email: 'admin@example.com', password: '12345678', password_confirmation: '12345678', beta_user: true)
+end
+
+unless AccessCode.where(code: 'test')
+  AccessCode.create(name: 'test', code: test, expires: false)
+end
+
