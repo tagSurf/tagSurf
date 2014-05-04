3b83cee191131844cfa211a765a60892972f5422
bweikulrich <bweikulrich@me.com>
2014-05-03 19:00:50 -0700

    override default create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < Devise::SessionsController
   
   layout 'client'
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
   
   def destroy
     reset_session
