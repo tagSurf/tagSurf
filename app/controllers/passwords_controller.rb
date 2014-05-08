5cd98c490611d41a76fdc3b62f489fcee6e5cf6d
bweikulrich <bweikulrich@me.com>
2014-05-08 16:21:33 -0700

    add back success path alerts

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,6 +8,8 @@ class PasswordsController < Devise::PasswordsController
     
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_flashing_format?
       sign_in(resource_name, resource)
       respond_with resource, location: after_resetting_password_path_for(resource)
     else
