f1e829d688f61d1275b645a5b48ca5eccc4dbeca
bweikulrich <bweikulrich@me.com>
2014-05-08 16:50:55 -0700

    reset password subject line

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,6 +8,7 @@ class PasswordsController < Devise::PasswordsController
     
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
+      flash.discard(:error)
       sign_in(resource_name, resource)
       respond_with resource, location: after_resetting_password_path_for(resource)
     else
