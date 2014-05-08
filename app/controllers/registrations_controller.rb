5061e877d8f7880a731b479d6373cfd3e7baaece
bweikulrich <bweikulrich@me.com>
2014-05-07 23:14:14 -0700

    remove uneeded devise message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -161,4 +161,23 @@ class RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.sanitize(:account_update)
   end
 
+  def require_no_authentication
+    assert_is_devise_resource!
+    return unless is_navigational_format?
+    no_input = devise_mapping.no_input_strategies
+
+    authenticated = if no_input.present?
+      args = no_input.dup.push scope: resource_name
+      warden.authenticate?(*args)
+    else
+      warden.authenticated?(resource_name)
+    end
+
+    if authenticated && resource = warden.user(resource_name)
+      redirect_to after_sign_in_path_for(resource)
+    end
+  end
+
+
+
 end
