eb7d51a93e0e4dc1bbf22527b5ede1dbc5887530
bweikulrich <bweikulrich@me.com>
2014-05-13 23:27:50 -0700

    direct users to a tagsurf page when not on device for some routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirect_desktops
-    unless browser.mobile? && current_user && current_user.admin? 
-      redirect_to '/desktop'
+    unless browser.mobile?
+      unless  current_user && current_user.admin? 
+        redirect_to '/desktop'
+      end
     end
   end 
 
