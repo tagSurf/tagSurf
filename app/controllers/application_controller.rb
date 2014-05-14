c355f0fdaacdcf32c05563c5b86eb2850c125e4b
bweikulrich <bweikulrich@me.com>
2014-05-14 08:22:23 -0700

    two nots, not one not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirect_desktops
-    if browser.mobile? || !browser.tablet?
+    if !browser.mobile? || !browser.tablet?
       unless  current_user && current_user.admin? 
         redirect_to '/desktop'
       end
