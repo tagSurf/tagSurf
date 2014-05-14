a11bb38f59faa6ce623bad28dcc13e3f08426a72
bweikulrich <bweikulrich@me.com>
2014-05-14 08:26:46 -0700

    flawed logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirect_desktops
-    if !browser.mobile? || !browser.tablet?
-      unless  current_user && current_user.admin? 
-        redirect_to '/desktop'
-      end
+    return if browser.mobile?
+    return if browser.tablet?
+    unless  current_user && current_user.admin? 
+      redirect_to '/desktop'
     end
   end 
 
