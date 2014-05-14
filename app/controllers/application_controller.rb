10a6a196f4903f7ed6dbb69be7723a9f77052cba
bweikulrich <bweikulrich@me.com>
2014-05-13 23:22:32 -0700

    desktop page for non-mobile users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :redirect_desktops
-
   def redirect_desktops
-    return unless Rails.env.production? && !browser.mobile? 
-    unless current_user && current_user.admin? 
-      return if request.path == '/device'
-      #redirect_to '/device'
+    unless browser.mobile? && current_user && current_user.admin? 
+      redirect_to '/desktop'
     end
   end 
 
