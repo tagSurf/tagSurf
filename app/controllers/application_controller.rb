24b89dd384dfffd22ceb7d1916b413bd96c0b872
bweikulrich <bweikulrich@me.com>
2014-05-14 08:16:20 -0700

    exclude tablets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirect_desktops
-    unless browser.mobile?
+    if browser.mobile? || !browser.tablet?
       unless  current_user && current_user.admin? 
         redirect_to '/desktop'
       end
