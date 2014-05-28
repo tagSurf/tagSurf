1d6f9eb00c947684d93d0fa2dbd777e908c2a6ac
bweikulrich <bweikulrich@me.com>
2014-05-27 23:41:26 -0700

    removes 2 file and duplicate seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,6 @@ unless Tag.where(name: 'imgurhot')
   Tag.create(name: 'imgurhot')
 end
 
-Media.populate_trending!
-
 unless User.where(email: 'admin@example')
   User.create(email: 'admin@example.com', password: '12345678', password_confirmation: '12345678', beta_user: true)
 end
