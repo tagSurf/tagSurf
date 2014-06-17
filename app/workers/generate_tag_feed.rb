6d4c6303fed71322016ffddfc22b303fe18e7033
bweikulrich <bweikulrich@me.com>
2014-06-17 14:12:21 -0700

    filters tags from main feed with no votes or no media

--- app/workers/generate_tag_feed.rb
+++ app/workers/generate_tag_feed.rb
@@ -4,20 +4,16 @@ class GenerateTagFeed
   def perform
     tags = Tag.all
     tag.each do |tag|
-
-      # Refresh all the tags
-      tag.tag_feed.delete(tag.name)
-
+      votes = Vote.where(vote_tag: tag.name).count
       if Tag.blacklisted?(tag.name)
         tag.tag_feed.delete(tag.name)
       elsif votes > 0
-        tag.tag_feed[tag.name] = Vote.where(vote_tag: tag.name).count
+        tag.tag_feed[tag.name] = votes
       elsif Media.tagged_with(tag.name, :wild => true).count > 0
         tag.tag_feed[tag.name] = 0
       else
-        next
+        tag.tag_feed.delete(tag.name)
       end
-
     end
   end
 
