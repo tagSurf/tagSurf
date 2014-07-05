ba9e418cf98a5012828cc1b48d25e5c0dbf84de2
bubbleboy14 <mario.balibrera@gmail.com>
2014-07-05 15:59:25 -0700

    Pointer cursor for prompt close button ('x').

--- public/js/util.js
+++ public/js/util.js
@@ -249,7 +249,7 @@ var featureBlockContents, buildFeatureBlockerContents = function() {
 		title = document.createElement('p'),
 		message = document.createElement('p'),
 		link = document.createElement('div');
-	closeContainer.className = "close_button_container";
+	closeContainer.className = "close_button_container pointer";
 	close.className = "x_close_button";
 	close.innerHTML = "X";
 	gesture.listen('down', closeContainer, modal.callPrompt);
