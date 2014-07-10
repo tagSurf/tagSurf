d6c8b0ea680f4970282436245b7775db2454babc
bweikulrich <bweikulrich@me.com>
2014-07-09 20:25:31 -0700

    users status api updates

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::UsersController < Api::BaseController
-
+    
+  before_action :find_authenticated_user, except: :stats
+  
   def paginated_history
     @offset = user_params["offset"].to_i
     @limit = user_params["limit"].to_i
@@ -79,14 +81,14 @@ class Api::UsersController < Api::BaseController
   end
 
   def stats
-    results = {:user => @user}
-    results['total_votes'] = @user.find_voted_items.count
-    results['up_votes']    = @user.find_up_voted_items.count
-    results['down_votes']  = @user.find_down_voted_items.count
-    if @user
+    if @user = current_user
+      results = {:user => @user}
+      results['total_votes'] = @user.find_voted_items.count
+      results['up_votes']    = @user.find_up_voted_items.count
+      results['down_votes']  = @user.find_down_voted_items.count
       render json: results
     else
-      render json: "no user found"
+      render json: "no active user"
     end
   end
 
