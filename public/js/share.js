f487e25d9254ea438440c02ef5e89dd5a5817e1a
VirtualPaul <VirtualPaul@Gmail.com>
2014-07-10 19:38:10 -0700

    special share url for tagsurf landing cards

--- public/js/share.js
+++ public/js/share.js
@@ -8,8 +8,12 @@ var share =
 		var hostname = document.location.hostname;
 		if (hostname.indexOf("localhost") != -1)
 			hostname = "beta.tagsurf.co";
-		return encodeURI("http://" + hostname + "/share/"
-			+ current_tag + "/" + share.data.id);
+		//Special share treatment for landing page cards
+		if (share.data.id == 272733 || share.data.id == 272738)
+			return encodeURI("http://tagsurf.co")
+		else 
+			return encodeURI("http://" + hostname + "/share/"
+				+ current_tag + "/" + share.data.id);
 	},
 	networks: {
 		facebook: function() {
