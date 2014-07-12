0465e973048075f7fd44dedd6149803219e931e0
k_fluff <drmartinlutherchang@gmail.com>
2014-07-11 19:52:42 -0700

    added scrolling of card with up and down arrows onkeydown the window increments/decrements the scroll top of the container by a factor that depends on a constant and an inertial variable that is reset onkeyup

--- public/js/feed.js
+++ public/js/feed.js
@@ -354,7 +354,23 @@ onload = function ()
 		// removed history slider
 //		addHistoryItem(activeCard);
 	};
+	var keyInertia = 0;
+	window.onkeydown = function(e) {
+		keyInertia += 1;
+		e = e || window.event;
+		var code = e.keyCode || e.which;
+		if (code == 38){
+			//boundary checking
+			scrollContainer.scrollTop -= (2 * keyInertia);
+		}
+		else if (code == 40){
+			//boundary checking
+			window.scrollBy(0,10);
+			scrollContainer.scrollTop += (2 * keyInertia);
+		}
+	};
 	window.onkeyup = function(e) {
+		keyInertia = 0;
 		e = e || window.event;
 		var code = e.keyCode || e.which;
 		if (code == 32){
@@ -387,7 +403,7 @@ onload = function ()
 				path: "/feed#"+current_tag,
 				referrer: 'http://beta.tagsurf.co/'
 			});
-		};
+		}
 	};
 	var swipeCallback = function (direction, distance, dx, dy, pixelsPerSecond)
 	{
