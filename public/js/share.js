52abc37d289c00a68d1363c58a92ca3ccf07a510
VirtualPaul <VirtualPaul@Gmail.com>
2014-07-13 19:09:58 -0700

    force large link returns in share.js

--- public/js/share.js
+++ public/js/share.js
@@ -31,8 +31,7 @@ var share =
 			});
 			return "https://www.facebook.com/dialog/feed"
 				+ "?app_id=676135635790285" + "&link=" + u
-				+ "&type=article"
-				// + "&picture=" + encodeURI(image.get(d, window.innerWidth - 40).url)
+				+ "&picture=" + encodeURI(image.get(d, 500).url)
 				+ "&name=" + encodeURI(d.caption)
 				+ "&description=%23" + share_tag
 				// + "&caption=" + document.location.hostname
