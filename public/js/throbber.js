9f69da49fa59b42df6ce1ea07f15e4845a9a1d94
VirtualPaul <VirtualPaul@Gmail.com>
2014-07-21 21:26:46 -0700

    add splashscreen back to loading throbber

--- public/js/throbber.js
+++ public/js/throbber.js
@@ -2,13 +2,18 @@ var throbber =
 {
 	active: false,
 	gif: document.createElement('img'),
+	back: document.createElement('div'),
 	_build: function ()
 	{
+		
 		throbber.gif.src = "/img/spin_throbber.gif";
 		throbber.gif.className = "throbber";
+		throbber.back.className = "splash-screen";
+		throbber.back.style['opacity'] = 0;
 		throbber.gif.onload = function() {
 			throbber.gif.style['left'] = ((window.innerWidth - throbber.gif.offsetWidth) / 2) + "px";
 		};
+		document.body.appendChild(throbber.back);
 		document.body.appendChild(throbber.gif);
 	},
 	on: function (addClass, addNode)
@@ -16,6 +21,7 @@ var throbber =
 		if (throbber.active)
 			return;
 		throbber.active = true;
+		throbber.back.style['opacity'] = 1;
 		if (addClass)
 		{
 			if (!throbber.gif.classList.contains(addClass))
@@ -27,7 +33,7 @@ var throbber =
 		else
 		{
 			throbber.gif.style['top'] = ((window.innerHeight - throbber.gif.offsetHeight) /2) + "px";
-		}
+		}	
 		throbber.gif.style['opacity'] = "1.0";
 	},
 	off: function ()
@@ -37,6 +43,7 @@ var throbber =
 			throbber.active = false;
 		})
 		throbber.gif.style['opacity'] = "0";
+		throbber.back.style['opacity'] = "0";
 	}
 };
 throbber._build();
