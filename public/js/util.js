2424b6e24e19bc44df1134c129e8369c60bdaecd
bubbleboy14 <mario.balibrera@gmail.com>
2014-07-28 23:38:04 -0700

    Autocomplete module adds additional wrapper element to target node and invokes drag.makeDraggable because iOS doesn't respect overflow-x hidden.

--- public/js/util.js
+++ public/js/util.js
@@ -266,8 +266,6 @@ var populateNavbar = function () {
       tag_adder.firstChild.value = "#";
     }
   });
-  drag.nativeScroll(document.getElementById("add-tag-autocomplete"),
-    { constraint: "horizontal" });
   add_icon = document.getElementById("add-icon");
   if (isAuthorized()) {
     document.getElementById("logout").onclick = function() {
