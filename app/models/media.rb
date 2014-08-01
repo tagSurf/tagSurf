1ef4414601610f563292766847a5a9cbc8d7b81f
bweikulrich <bweikulrich@me.com>
2014-07-31 21:28:29 -0700

    typo in comment

--- app/models/media.rb
+++ app/models/media.rb
@@ -89,7 +89,7 @@ class Media < ActiveRecord::Base
 
   # Gather the next set of media for feeds 
   # The brains of tagSurf feeds 
-  # TODO evaluate for effeciency
+  # TODO evaluate for efficiency
   def self.next(user, tag, options = {})
     offset = options[:offset].nil? ? 0 : options[:offset].to_i
     n = options[:limit].nil? ?  20 : options[:limit].to_i
