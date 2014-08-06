e4dbb0fa4a5a874fa570970e7f04956941b966bc
VirtualPaul <VirtualPaul@Gmail.com>
2014-08-06 15:58:25 -0700

    refactor keyboard gestures with stroke.js

--- public/js/stroke.js
+++ public/js/stroke.js
@@ -2,29 +2,43 @@ var stroke = {
 	keys: {},
 	cbs: { up: {}, down: {} },
 	init: function() {
-		console.log("stroke.init");
 		window.onkeydown = function (e) {
 			e = e || window.event;
-			var code = e.keyCode || e.which;
-			keys[code] = { down: Date.now() };
-			cbs.down[code](keys[code]);
-		}
+			var code = e.keyCode || e.which,
+				ac = stroke.cbs.down.always,
+				cb = stroke.cbs.down[code],
+				now = Date.now(),
+				kobj = stroke.keys[code] =
+					(stroke.keys[code] && !stroke.keys[code].up)
+					? stroke.keys[code] : { duration: 0 };
+			if (kobj.down)
+				kobj.duration += (now - kobj.down);
+			kobj.down = now;
+			ac && ac(kobj);
+			cb && cb(kobj);
+		};
 		window.onkeyup = function (e) {
 			e = e || window.event;
 			var code = e.keyCode || e.which,
-				keyobj = keys[code];
-			keyobj.up = Date.now();
-			keyobj.duration = keyobj.up - keyobj.down;
-			cbs.up[code](keyobj);
-		}
+				kobj = stroke.keys[code],
+				ac = stroke.cbs.up.always,
+				cb = stroke.cbs.up[code];
+			kobj.up = Date.now();
+			kobj.duration += kobj.up - kobj.down;
+			ac && ac(kobj);
+			cb && cb(kobj);
+		};
+	},
+	isDown: function(keyCode) {
+		var kobj = stroke.keys[keyCode],
+			isDown = kobj && !kobj.up;
+		return isDown;
 	},
 	listen: function(evt, keyCode, cb) {
-		console.log("stroke.listen:", evt, keyCode, cb);
-		cbs[evt][keyCode] = cb;
+		stroke.cbs[evt][keyCode || 'always'] = cb;
 	},
 	unlisten: function(evt, keyCode) {
-		console.log("stroke.unlisten:", evt, keyCode);
-		delete cbs[evt][keyCode];
+		delete stroke.cbs[evt][keyCode || 'always'];
 	}
 };
 stroke.init();
