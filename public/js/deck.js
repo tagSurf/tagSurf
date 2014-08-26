4c8eb4bdea33c9b18f7e5563314f5e25c2d68a4e
bubbleboy14 <mario.balibrera@gmail.com>
2014-08-26 12:44:44 -0700

    Determine shareDeck value in getDeck() instead of dataPath(). Removed cardsToLoad, building (now 'refilling'), and build_retry from deck. Replaced elaborate build() with refill(), which issues the ajax request and passes the results (cardized and filtered) to image.load(). Refactored getDeck() to load first card if there is one and refill the deck. Replaced complex deal() with small function that places cards up to stack_depth in slider and turns off the throbber if a top card is present. Removed promotion and demotion logic. Deck.refresh() calls refill() if cards.length + image.loadCount() < buffer_minimum. Removed preloadCards(), now a one-liner invoked in refill(). Added cardLoaded callback, triggered by image loader, which pushes newly-loaded cards onto deck.cards and calls deal(). Simplified popData filters out old cards and records freshly known keys.

--- public/js/deck.js
+++ public/js/deck.js
@@ -9,87 +9,44 @@ var deck_proto = {
 		return this.cards[0];
 	},
 	popData: function(rdata) {
-		var i, starters = [], others = [], preloads = [];
-		if (!isAuthorized()) {
-			for (i = 0; i < rdata.length; i++) {
-				if ((!this.known_keys[rdata[i].id] && !this.voted_keys[rdata[i].id]) || rdata[i].type == "login")
-						preloads.push(rdata[i]);
+		var i, d, preloads = [];
+		for (i = 0; i < rdata.length; i++) {
+			d = rdata[i];
+			if ((!this.known_keys[d.id] && !this.voted_keys[d.id]) || d.type == "login") {
+				this.known_keys[d.id] = true
+				preloads.push(d);
 			}
 		}
-		else {
-			for (i = 0; i < rdata.length; i++) {
-				if (!this.known_keys[rdata[i].id] && !this.voted_keys[rdata[i].id]) {
-					var d = rdata[i];
-					((!d.animated && starters.length < this.constants.stack_depth)
-						? starters : others).push(d);
-					this.known_keys[d.id] = true
-				}
-			}
-			for (i = 0; i < starters.length; i++) preloads.push(starters[i]);
-			for (i = 0; i < others.length; i++) preloads.push(others[i]);
-		}
-		this.cards = this.cards.concat(preloads);
 		return preloads;
 	},
-	preloadCards: function(num) {
-		if (!this.cardsToLoad.length)
-			return;
-		var size = this.cardsToLoad.length < num ? this.cardsToLoad.length : num;
-		size = size ? size : this.cardsToLoad.length;
-		image.load(this.cardsToLoad.splice(0, size), window.innerWidth - 40);
-		if (DEBUG)	
-			console.log("preload " + size + " cards");
+	cardLoaded: function(c) {
+		this.cards.push(c);
+		this.deal();
 	},
-	dataPath: function(firstCard) {
+	dataPath: function() {
 		if (!isAuthorized()) {
 			var p = "/api";
-			this.shareDeck = true;
-			if (firstCard || this.tag
+			if (this.firstCard || this.tag
 				!= document.location.pathname.split("/")[2])
 				p += "/share/" + this.tag + "/" +
-					(firstCard ? firstCard.id : 0);
+					(this.firstCard ? this.firstCard.id : 0);
 			else
 				p += document.location.pathname;
 			return p + "/20/" + (this.shareOffset++ * 20);
 		}
 		return "/api/media/" + this.tag;
 	},
-	build: function (update, firstCard) {
-		if (this.building)
+	refill: function () {
+		if (this.refilling)
 			return;
 		var self = this;
-		self.building = true;
-		if (DEBUG)
-			console.log("deck.build, update = " + update + " firstCard = ", firstCard);
-		xhr(this.dataPath(firstCard), null, function(response_data) {
-			var rdata = response_data.data.map(newCard);
-			self.cardsToLoad = self.cardsToLoad.concat(self.popData(rdata));
-			self.building = false;
-			self.build_retry = false;
-			if (update)
-				self.preloadCards();	
-			else {
-				self.preloadCards(self.constants.stack_depth);
-				if (self.shareDeck)
-					self.spaceLoginCards();
-			}
-		}, function(response, status) {
-			if (status == 401){
-				self.building = false;
-				messageBox("Oops", response.errors 
-					+ "<br/><br/><i>Control Safe Surf from Options</i>");
-				self.deal();
-				return;
-			}
-			if (DEBUG)
-				console.log("deck.build xhr error");
-			self.building = false;
-			self.build_retry = !self.build_retry
-			if (self.build_retry){
-				self.deal();
-				throbber.active && throbber.off();	
-				self.refresh();
-			}
+		self.refilling = true;
+		xhr(this.dataPath(), null, function(response_data) {
+			self.refilling = false;
+			image.load(self.popData(response_data.data.map(newCard)),
+				window.innerWidth - 40, function(c) {
+					self.cardLoaded(c);
+				});
 		});
 	},
 	skipTutorial: function() {
@@ -105,9 +62,8 @@ var deck_proto = {
 		this.cards = this.cards.filter(function(card) {
 			return !deck_proto.voted_keys[card.id];
 		});
-		if (this.shareDeck) {
+		if (this.shareDeck)
 			this.spaceLoginCards();
-		}
 	},
 	removeLoginCards: function () {
 		this.cards = this.cards.filter(function(card){
@@ -138,135 +94,18 @@ var deck_proto = {
 			this.refresh();
 	},
 	refresh: function() {
-		this.preloadCards();
-		if (this.cards.length < this.constants.buffer_minimum)
-			this.build(true);
-	},
-	demoteCard: function (c, positions) {
-		if (this.cards.indexOf(c) != -1) {
-			var currentIndex = this.cards.indexOf(c),
-				newIndex = currentIndex + positions < this.cards.length - 1 ? 
-					currentIndex + positions : this.cards.length - 1; 
-			for (var i = 0; i < (newIndex - currentIndex); i++)
-				c.demote();
-			// splice card from current position and insert it into new index with 0 removals
-			this.cards.splice(newIndex, 0, this.cards.splice(currentIndex, 1)[0]);
-			if (currentIndex < this.constants.stack_depth)
-				this.deal();
-		}
-	},
-	promoteCard: function (c, positions) {
-		if (this.cards.indexOf(c) != -1) {
-			var currentIndex = this.cards.indexOf(c),
-				newIndex = currentIndex - positions >= 0 ? currentIndex - positions : 0; 
-			for (var i = currentIndex; i > (currentIndex - newIndex); i--)
-				c.promote();
-			// splice card from current position and insert it into new index with 0 removals
-			this.cards.splice(newIndex, 0, this.cards.splice(currentIndex, 1)[0]);
-			if (newIndex < this.constants.stack_depth)
-				this.deal();
-		}
+		if (this.cards.length + image.loadCount() < this.constants.buffer_minimum)
+			this.refill();
 	},
 	deal: function() {
-		// deck dealer
-		if (this != current_deck)
-			return;
-		var cardbox = document.getElementById("slider"),
-			self = this;
-		// If the deck is building and there are no cards to deal
-		if (this.building && (this.cards.length < 1 || this.topCard().type == "waves")) {
-			// Delay deal until update is complete
-			setTimeout(function() { self.deal(); }, 3000)
-			if(DEBUG)
-				console.log("Delay deal because deck is building");
-			return;
+		var i, c, slider = document.getElementById("slider");
+		for (i = slider.childNodes.length; i < this.constants.stack_depth; i++) {
+			c = this.cards[i];
+			if (!c) break;
+			c.show();
 		}
-		// If no topCard, there is nothing to deal
-		if (!this.topCard())
-			return;
-		// If topCard is an End-Of-Feed card with no cards behind it and it's showing there is nothing to deal
-		if (this.cards.length == 1 && this.topCard().type == "End-Of-Feed" && this.topCard().showing)
-			return;
-		// If topCard is waves and there are no cards left in the deck set End-Of-Feed
-		else if (this.cards.length == 1 && this.topCard().type == "waves")
-			this.topCard().setFailMsg();
-		// If there are cards in the deck and the topCard is waves or End-Of-Feed remove it
-		if (this.cards.length > 1 && (this.topCard().type == "waves" || this.topCard().type == "End-Of-Feed")) {
-			if(DEBUG)
-				console.log("Removed top card #" + this.topCard().id + " cards.length = " + this.cards.length + " cardbox.length = " + cardbox.childNodes.length + " card.surfsUp = " + this.topCard().surfsUp + " card = ", this.topCard());
-			this.topCard().remove();
-		}
-		// If topCard isn't showing, show it on top of stack
-		!this.topCard().showing && this.topCard().show(this.cardCb, this.constants.stack_depth);
-		// If topCard needs promoting...
-		if (this.topCard().zIndex < this.constants.stack_depth) {
-			var zIndexCatchUp = this.constants.stack_depth - this.topCard().zIndex;
-			for (var i = 0; i < zIndexCatchUp; i++) {
-				// Promote all visible cards as many times as topCard needs promoting
-				for (var f = 0; f < cardbox.childNodes.length; f++)
-					this.cards[f] && this.cards[f].showing && this.cards[f].promote();
-			}
-		}
-		// If second card needs promoting
-		if (this.cards[1] && this.cards[1].zIndex < this.constants.stack_depth - 1)
-			// promote it 
-			for (var i = 1; i < cardbox.childNodes.length; i++)
-				this.cards[i] && this.cards[i].showing && this.cards[i].promote();
-		// For as many cards as are missing from the table
-		for (var i = cardbox.childNodes.length; i < this.constants.stack_depth; i++) {
-			var c = this.cards[i];
-			// If this is end of deck and waves or End-Of-Feed card is last one, end deal
-			if (!c && this.cards[i - 1] && (this.cards[i - 1].type == "End-Of-Feed" 
-				|| this.cards[i - 1].type == "waves")) {
-				if(DEBUG)				
-					console.log("Skip deal because reached end of cards and last card is set");
-				return;
-			} 
-			// If there is a waves card with another card behind it, remove it
-			else if (c && c.type == "waves" && this.cards[i + 1]) {
-				c.remove(null);
-				//If the new card isn't showing
-				if (!this.cards[i].showing)
-					// Show it
-					this.cards[i].show(this.cardCbs, this.constants.stack_depth - i)
-				else
-					// If it's showing, promote it and all cards behind it
-					for (var f = i; f < cardbox.childNodes.length; f++)
-						this.cards[f] && this.cards[f].showing && this.cards[f].promote();
-			} 
-			// If there is not a card and the previous card is not waves
-			else if (!c) {
-				// Make a new waves card and show it, then end the deal
-				c = this.cards[i] = newCard();
-				if (DEBUG)
-					console.log("Create new throbber card, i = " + i + " cards.length = " + this.cards.length + " cardbox.length = " + cardbox.childNodes.length);
-				c.show();
-				return;
-			} 
-			// Show the next card in the deck
-			else 
-				c.show(this.cardCbs, this.constants.stack_depth - i);
-		}
-		// If the second and third cards do not have populated images
-		if (!this.shareDeck && this.cards[1] && this.cards[1].surfsUp && this.cards[1].type == "content" 
-			&& ((this.cards[2] && !this.cards[2].surfsUp) || (this.cards[3] && !this.cards[3].surfsUp))) {
-			// If there are more cards in the deck than on the table
-			if (this.cards.length > this.constants.stack_depth) {
-				if (DEBUG)
-					console.log("Punted card #" + this.cards[1].id + " because it wasn't ready to be shown");
-				// Send the unpopulated card to back of deck to give time for image load
-				this.demoteCard(this.cards[1], this.cards.length - 1);
-			} 
-			// If what's on the table is all that's left, remove the unpopulated card
-			else {
-				this.cards[1].remove();
-				if (DEBUG)
-					console.log("Removed card #" + this.cards[1].id + " because it wasn't ready to be shown");
-			}
-		}
-		// Turn throbber off
-		throbber.active && throbber.off();
-	}
+		this.topCard() && throbber.off();
+	},
 };
 
 var cardDecks = {};
@@ -281,23 +120,16 @@ var getDeck = function(tag, firstCard, cardCbs){
 	deck.cardCbs = cardCbs;
 	deck.tag = tag;
 	deck.known_keys = {};
-	deck.shareDeck = false;
+	deck.shareDeck = !isAuthorized();
 	deck.shareOffset = 0;
 	deck.cards = [];
-	deck.cardsToLoad = [];
-	deck.building = false;
-	deck.build_retry = false;	
 	if (firstCard) {
-		deck.cards[0] = firstCard;
+		deck.cards[0] = deck.firstCard = firstCard;
 		deck.known_keys[firstCard.id] = true;
-		deck.deal();
-		deck.build(true);
+		image.load(deck.cards, window.innerWidth - 40,
+			function() { deck.deal(); });
 	}
-	else {
-		var c = deck.cards[0] = newCard();
-		c.show();
-		deck.build(false);
-	}
+	deck.refill();
 	return deck;
 };
 var removeFromDecks = function(c) {
