29a2ed0dedcb7126f88e0fe18d77d309b146901f
VirtualPaul <VirtualPaul@Gmail.com>
2014-09-01 12:20:53 -0700

    disable tutorial until ready to go live

--- public/js/feed.js
+++ public/js/feed.js
@@ -597,8 +597,8 @@ onload = function ()
 
 	analytics.identify(currentUser.id);
 
-	if (!isAuthorized() && !DEBUG)
-		tutorial.start();
+	// if (!isAuthorized() && !DEBUG)
+	// 	tutorial.start();
 };
 
 //This is the first line executed in feed
