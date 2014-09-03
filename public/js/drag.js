33d5956352354fc55d2134f75ca3e39aa1eb2f89
bubbleboy14 <mario.balibrera@gmail.com>
2014-09-02 22:23:38 -0700

    Refactored drag module to track finger motion and match node movement via velocity-degrading request animation frame tick.

--- public/js/drag.js
+++ public/js/drag.js
@@ -1,12 +1,46 @@
 var drag = {
-	_direction2constraint: {
-		up: "horizontal",
-		down: "horizontal",
-		left: "vertical",
-		right: "vertical"
+	// i'm defining some stuff here that could
+	// instead be derived programmatically.
+	// the idea is to reduce run-time load by
+	// avoiding things like string concatonations
+	// at potentially critical junctures like bound().
+	// maybe it won't end up mattering. we'll see.
+	//  - mario
+	_axes: {
+		horizontal: {
+			velocity: "vx",
+			drag: "xDrag",
+			clientSize: "clientWidth",
+			scrollSize: "scrollWidth",
+			directions: ["right", "left"]
+		},
+		vertical: {
+			velocity: "vy",
+			drag: "yDrag",
+			clientSize: "clientHeight",
+			scrollSize: "scrollHeight",
+			directions: ["down", "up"]
+		}
 	},
-	nativeScroll: function (n, opts)
-	{
+	_direction2axis: {
+		up: "vertical",
+		down: "vertical",
+		left: "horizontal",
+		right: "horizontal"
+	},
+	_direction2opposite: {
+		up: "down",
+		down: "up",
+		right: "left",
+		left: "right"
+	},
+	constants: {
+		snappiness: 5, // less is more!
+		minVelocity: 0.01,
+		velocityDecay: 0.6,
+		swipeMultiplier: 10
+	},
+	nativeScroll: function (n, opts) {
 		gesture.listen("up", n, function () {
 			if (opts.up)
 				opts.up();
@@ -17,18 +51,13 @@ var drag = {
 				opts.down();
 			return true;
 		}, true, false);
-		var dirs = {
-			up: "down",
-			down: "up",
-			right: "left",
-			left: "right"
-		}, lastDirection, dragTimeout, delayedDrag = function() {
+		var lastDirection, dragTimeout, delayedDrag = function() {
 			if (dragTimeout) {
 				clearTimeout(dragTimeout);
 				dragTimeout = null;
 			}
 			dragTimeout = setTimeout(function() {
-				opts.drag(dirs[lastDirection], 0, 0, 0);
+				opts.drag(drag._direction2opposite[lastDirection], 0, 0, 0);
 			}, 100);
 		};
 		gesture.listen("drag", n, function (direction, distance, dx, dy, velocity, vx, vy) {
@@ -41,7 +70,7 @@ var drag = {
 				(atBottom && direction == "up"))
 				return false;
 			return !opts.constraint ||
-				opts.constraint == drag._direction2constraint[direction];
+				opts.constraint != drag._direction2axis[direction];
 		}, true, false);
 		gesture.listen("swipe", n, function (direction, distance, dx, dy, velocity, vx, vy) { 
 			if (direction == "up" && (n.parentNode.scrollTop >=
@@ -61,8 +90,7 @@ var drag = {
 		n.parentNode.addEventListener('scroll', n.parentNode._ndcb, false);
 		n.parentNode.isNativeDraggable = true;
 	},
-	makeDraggable: function (node, opts)
-	{
+	makeDraggable: function (node, opts) {
 		opts = opts || {};
 		if (node.isCustomDraggable) {
 			gesture.unlisten(node);
@@ -74,7 +102,7 @@ var drag = {
 		if (!opts.interval && !opts.force && !isStockAndroid())
 			return drag.nativeScroll(node.firstChild, opts);
 		var downCallback, upCallback, dragCallback, swipeCallback,
-			dirs, bound, triggerCbs, translate;
+			bounds, triggerCbs, currentDirection, rAF_drag;
 		node.xDrag = 0;
 		node.yDrag = 0;
 		node.classList.add('hardware-acceleration');
@@ -85,166 +113,108 @@ var drag = {
 			node.parentNode.style.overflow = "visible";
 		};
 		node.parentNode.addEventListener('scroll', returnFalse, false);
-		dirs = {
-			horizontal: {
-				drag: "xDrag",
-				dimension: "Width",
-				direction: ["right", "left"]
-			},
-			vertical: {
-				drag: "yDrag",
-				dimension: "Height",
-				direction: ["up", "down"]
-			}
-		};
-		bound = function (mult) {
-			var direction;
-			for (var dir in dirs) {
-				var dirdata = dirs[dir];
-				if (opts.constraint != dir) {
-					var dragPos = node[dirdata.drag],
-						dim = dirdata.dimension,
-						outerBound = (mult || 1)
-							* node.parentNode["client" + dim]
-							- node["scroll" + dim];
-					if (dragPos > 0) {
-						node[dirdata.drag] = 0;
-						direction = dirdata.direction[0];
-					} else if (dragPos < outerBound) {
-						node[dirdata.drag] = outerBound;
-						direction = dirdata.direction[1];
-					}
-				}
-			}
-			return direction;
-		};
-		triggerCbs = function (direction, distance, dx, dy) {
+
+		triggerCbs = function (direction, distance, dx, dy, velocity, vx, vy) {
 			if (opts.drag) 
-				opts.drag(direction, distance || 0, dx || 0, dy || 0);
+				opts.drag(direction, distance || 0, dx || 0, dy || 0,
+					velocity || 0, vx || 0, vy || 0);
 			if (opts.scroll)
 				opts.scroll();
 		};
-		translate = function (cb) {
+		bounds = function() {
+			return {
+				vertical: node.parentNode.clientHeight - node.scrollHeight,
+				horizontal: node.parentNode.clientWidth - node.scrollWidth
+			}
+		};
+		canGo = function(axis) {
+			if (opts.constraint == axis) return false;
+			var axisdata, dragPos, outerBound;
+			axisdata = drag._axes[axis];
+			dragPos = node[axisdata.drag];
+			outerBound = bounds()[axis];
+			return currentDirection != (dragPos > 0 ? axisdata.directions[0]
+				: dragPos < outerBound ? axisdata.directions[1] : null);
+		};
+		rAF_drag = function () {
+			var axis, axisdata,
+				time = Date.now(),
+				dt = (time - node.time);
+			for (axis in drag._axes) {
+				axisdata = drag._axes[axis];
+				if (canGo(axis))
+					node[axisdata.drag] = Math.min(Math.max(bounds()[axis],
+						node[axisdata.drag] + node[axisdata.velocity] * dt), 0);
+				else
+					node[axisdata.velocity] = 0;
+				node[axisdata.velocity] *= drag.constants.velocityDecay;
+				if (node[axisdata.velocity] && Math.abs(node[axisdata.velocity])
+					< drag.constants.minVelocity)
+					node[axisdata.velocity] = 0;
+			}
+			node.time = time;
 			node.style['-webkit-transform'] =
-				"translate3d(" + node.xDrag + "px,"
-				+ node.yDrag + "px,0)";
-			if (cb) {
-				node.animating = true;
-				trans(node, function() {
-					node.animating = false;
-					(typeof cb == "function") && cb();
-				}, "-webkit-transform 300ms ease-out");
+				"translate3d(" + node.xDrag + "px," + node.yDrag + "px,0)";
+			if (node.vx || node.vy)
+				node.rAFid = requestAnimFrame(rAF_drag);
+			else {
+				node.rAFid = null;
+				triggerCbs(currentDirection);
 			}
 		};
 		downCallback = function () {
 			if (node.animating) return;
 			node.dragging = false;
 			node.touchedDown = true;
-			node.animating = false;
-			node.xDragStart = node.xDrag;
-			node.yDragStart = node.yDrag;
 			if (opts.down)
 				opts.down();
 		};
-		upCallback = function (direction) {
+		upCallback = function () {
 			node.touchedDown = node.dragging = false;
 			if (node.animating == false) {
 				if (opts.interval) {
-					for (var dir in dirs) {
-						var dirdata = dirs[dir];
+					for (var dir in drag._axes) {
+						var axisdata = drag._axes[dir];
 						if (opts.constraint != dir) {
-							var mod = node[dirdata.drag] % opts.interval;
+							var mod = node[axisdata.drag] % opts.interval;
 							if (mod) {
-								var dragStart = node[dirdata.drag + "Start"];
-								node[dirdata.drag] -= (Math.abs(mod)
-									<= (opts.interval / 2))
+								var dragStart = node[axisdata.drag + "Start"];
+								node[axisdata.drag] += node[axisdata.drag]
+									- (Math.abs(mod) <= (opts.interval / 2))
 									? mod : (opts.interval + mod);
-								if (node[dirdata.drag] < dragStart)
-									direction = dirdata.direction[0];
-								else if (node[dirdata.drag] > dragStart)
-									direction = dirdata.direction[1];
-								else
-									direction = "hold";
 							}
 						}
 					}
-					if (direction)
-						translate(true);
-				} else {	//boundary checking
-					var boundaryDirection = bound();
-					if (boundaryDirection) {
-						direction = boundaryDirection;
-						translate(function () { triggerCbs(direction); });
-					}
 				}
 				if (opts.up)
-					opts.up(direction);
+					opts.up();
 			}
 		};
-		dragCallback = function (direction, distance, dx, dy) {
+		dragCallback = function (direction, distance, dx, dy, velocity, vx, vy) {
 			if (node.touchedDown) {
 				node.dragging = true;
-				var boundaryDirection = bound();
-				if (opts.constraint != "vertical"
-					&& boundaryDirection != "up"
-					&& boundaryDirection != "down")
-					node.yDrag += dy;
-				if (opts.constraint != "horizontal"
-					&& boundaryDirection != "right"
-					&& boundaryDirection != "left")
-					node.xDrag += dx;
-				translate();
-				triggerCbs(direction, distance, dx, dy);
+				if (!node.rAFid) {
+					node.time = Date.now();
+					node.rAFid = requestAnimFrame(rAF_drag);
+				}
+				node.vx = Math[vx < 0 ? "min" : "max"](node.vx || 0, vx);
+				node.vy = Math[vx < 0 ? "min" : "max"](node.vy || 0, vy);
+				if (opts.constraint != drag._direction2axis[direction])
+					currentDirection = direction;
 			}
 		};
-		swipeCallback =  function (direction, distance, dx, dy, velocity, vx, vy)
-		{
-			var xMod = opts.interval ? node.xDrag % opts.interval : -dx;
-			var yMod = opts.interval ? node.yDrag % opts.interval : velocity * .3;
-			if (node.animating == false)
-			{
-				if (opts.constraint != "horizontal" && node.xDrag <= 0 && 
-					Math.abs(node.xDrag) < (node.scrollWidth - 
-					node.parentNode.clientWidth))
-				{
-					if (direction == "right")
-					{
-						node.xDrag -= xMod;
-					}
-					else if (direction == "left")
-					{
-						node.xDrag += (opts.interval ? -(opts.interval + xMod) : xMod);
-					}
-					else
-					{
-						return;
-					}
-				}
-				if (opts.constraint != "vertical" && node.yDrag <= 0 
-					&& node.yDrag > -(node.scrollHeight - 
-					node.parentNode.clientHeight))
-				{
-					if (direction == "up")
-					{
-						node.yDrag -= yMod;
-					}
-					else if (direction == "down")
-					{
-						node.yDrag -= (opts.interval ? -(opts.interval + yMod) : -yMod);
-					}
-					else
-					{
-						return;
-					}
-				}
-				translate(function() { upCallback(direction); });
-			}
+		swipeCallback = function (direction, distance, dx, dy, velocity, vx, vy) {
+			if (opts.constraint == drag._direction2axis[direction])
+				return;
+			currentDirection = direction;
+			node.vx = vx * drag.constants.swipeMultiplier;
+			node.vy = vy * drag.constants.swipeMultiplier;
 		};
 
 		node.isCustomDraggable = true;
 		gesture.listen("drag", node, dragCallback);
-		gesture.listen("down", node, downCallback);
 		gesture.listen("swipe", node, swipeCallback);
+		gesture.listen("down", node, downCallback);
 		gesture.listen("up", node, upCallback);
 	}
 };
