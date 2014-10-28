b8775418c3c2e71246971e16ddba38a64a1b5010
VirtualPaul <VirtualPaul@Gmail.com>
2014-10-27 21:46:54 -0700

    ad migration to update scores

---
+++ db/migrate/20141027214842_fix_scores.rb
@@ -0,0 +1,25 @@
+class FixScores < ActiveRecord::Migration
+  def change
+    Media.select(
+    	:id,
+    	:created_at,
+    	:remote_score
+    	).where(:time_bonus_expired => false).where(:created_at => 3.days.ago..Time.now).find_each(batch_size: 10000) do |m|
+    	score = m.remote_score + m.created_at.to_i - 1300000000 + (m.up_votes.to_i * 1000000)
+    	m.update_column('ts_score', score)
+    end
+    Media.select(
+    	:id,
+    	:created_at,
+    	:remote_score
+    	).where(:time_bonus_expired => false).where(:created_at => 7.days.ago..3.days.ago).find_each(batch_size: 10000) do |m|
+    	score = m.remote_score.to_i + m.created_at.to_i - 1350000000 + (m.up_votes.to_i * 1000000)
+    	puts "created_at #{m.created_at.to_i} up_votes #{m.up_votes.to_i}"
+    	m.update_column('ts_score', score)
+    end
+    Media.select(:id, :remote_score).where(:time_bonus_expired => true).find_each(batch_size: 10000) do |m|
+    	score = m.remote_score.to_i + (m.up_votes.to_i * 1000000)
+    	m.update_column('ts_score', score)
+    end
+  end
+end
