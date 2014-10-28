fb0cb9de424c543a94b25143bc4a8d1f17cd1d5b
VirtualPaul <VirtualPaul@Gmail.com>
2014-10-28 13:56:54 -0700

    rm vote counter init from migration

--- db/migrate/20141027214842_fix_scores.rb
+++ db/migrate/20141027214842_fix_scores.rb
@@ -5,7 +5,6 @@ class FixScores < ActiveRecord::Migration
     	:created_at,
     	:remote_score
     	).where(:time_bonus_expired => false).where(:created_at => 3.days.ago..Time.now).find_each(batch_size: 10000) do |m|
-    	m.init_vote_counter!
     	score = m.remote_score + m.created_at.to_i - 1300000000 + (m.up_votes.to_i * 1000000)
     	m.update_column('ts_score', score)
     end
@@ -14,12 +13,10 @@ class FixScores < ActiveRecord::Migration
     	:created_at,
     	:remote_score
     	).where(:time_bonus_expired => false).where(:created_at => 7.days.ago..3.days.ago).find_each(batch_size: 10000) do |m|
-    	m.init_vote_counter!
     	score = m.remote_score.to_i + m.created_at.to_i - 1350000000 + (m.up_votes.to_i * 1000000)
     	m.update_column('ts_score', score)
     end
     Media.select(:id, :remote_score).where(:time_bonus_expired => true).find_each(batch_size: 10000) do |m|
-    	m.init_vote_counter!
     	score = m.remote_score.to_i + (m.up_votes.to_i * 1000000)
     	m.update_column('ts_score', score)
     end
