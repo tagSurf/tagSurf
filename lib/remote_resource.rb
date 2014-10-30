6c6b8292f53ffe256e30a513406f7653fc60a683
VirtualPaul <VirtualPaul@Gmail.com>
2014-10-30 12:38:52 -0700

    begin instagram integration with updates to remote_resource

--- lib/remote_resource.rb
+++ lib/remote_resource.rb
@@ -1,16 +1,29 @@
 require 'httparty'
+require 'openssl'
+require 'base64'
 
 class RemoteResource
   include HTTParty
 
-  def self.get_request(service)
-    HTTParty.get(
-      "https://imgur-apiv3.p.mashape.com/3/#{service}",
-      :headers => {
-        "Authorization" => "Client-ID #{ENV['TS_IMGUR']}",
-        "X-Mashape-Authorization" => ENV['TS_MASHAPE']
-      }
-    )
+  def self.get_request(service, provider)
+    if provider = 'imgur'
+      HTTParty.get(
+        "https://imgur-apiv3.p.mashape.com/3/#{service}",
+        :headers => {
+          "Authorization" => "Client-ID #{ENV['TS_IMGUR']}",
+          "X-Mashape-Authorization" => ENV['TS_MASHAPE']
+        }
+      )
+    elsif provider = 'instagram'
+      digest = OpenSSL::Digest::Digest.new('sha256')
+      signature = OpenSSL::HMAC.hexdigest(digest, ENV['TS_INSTAGRAM'], ENV['SERVER_IP'])
+      HTTParty.get(
+        "https://api.instagram.com/v1/tags/#{service}/media/recent?access_token=#{ACCESS-TOKEN}",
+        :headers => {
+          "X-Insta-Forwarded-For" => "#{ENV[SERVER_IP]}|#{ENV['TS_INSTAGRAM']}"
+        }
+      )
+    end
   end
 
   def self.viral_feed
