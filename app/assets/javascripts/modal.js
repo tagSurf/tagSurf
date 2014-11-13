4fd690c0214ec95bfe207f1f851dd5ab1a1f8a39
VirtualPaul <VirtualPaul@Gmail.com>
2014-11-13 13:29:10 -0800

    only use weblinks for now

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -316,7 +316,7 @@ var modal = {
 	webIn: function (card, cb) {
 		var iframe = modal.web.firstChild.firstChild;
 		modal.web.out = true;
-		iframe.src = "http://imgur.com/gallery/" + card.data.remote_id;
+		iframe.src = card.data.web_link
 		modal.web.cb = cb || modal.webOut;
 		modal.web.style.display = "block";
 		gesture.preventDefault = false;
