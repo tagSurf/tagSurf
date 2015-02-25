df31d2f4d9ce550f8804c51861bc4f06798c9bc2
VirtualPaul <VirtualPaul@Gmail.com>
2015-02-25 00:05:19 -0800

    adjustments to mandrill integration

--- app/mailers/refer_mailer.rb
+++ app/mailers/refer_mailer.rb
@@ -8,6 +8,6 @@ class ReferMailer < ActionMailer::Base
     @username = @email.split("@")[0]
     @media = media
     @url = Rails.env.production? ? "http://beta.tagsurf.co" : "http://localhost:3000"
-    mail(to: @email, subject: "#{@referrer_name} has recommended something for you on tagSurf!")
+    mail(to: @email, subject: "#{@referrer_name} has recommended something for you on tagSurf!", from_email: "no-reply@tagsurf.co", async: "true", from_name: "tagSurf")
   end
 end
