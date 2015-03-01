6306bfe21bcf9198b24c8e484aba3316ac6d1ee8
VirtualPaul <VirtualPaul@Gmail.com>
2015-03-01 14:22:04 -0800

    return de-duped media collection of all user's referrals, needs ordering

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -14,12 +14,63 @@ class Referral < ActiveRecord::Base
   after_commit :find_vote, 	on: :create
   after_commit :send_notification, on: :create
 
-  def self.paginated_collection(user_id, limit, offset, safe)
-    if safe
-      Media.joins(:referrals).where("referrals.referrer_id = #{user_id}").nsfw(false).order('referrals.id desc').limit(limit).offset(offset)
-    else
-      Media.joins(:referrals).where("referrals.referrer_id = #{user_id}").order('referrals.id desc').limit(limit).offset(offset)
+  
+# Return referrals made BY a user
+  def self.paginated_collection(user_id, limit, offset, safe, prev_keys = nil)
+    known_keys = prev_keys ? prev_keys : Array.new
+    media_ids = Array.new
+    @offset = offset
+    @limit = limit
+
+    # Get media ids and de-dupe collection
+    while media_ids.length < limit do
+      if !media_ids.empty?
+        @limit = limit - media_ids.length
+      end
+
+      if !known_keys.empty?
+        keys = Referral.unscoped.select(:referrable_id, :created_at, :referrer_id).where(referrer_id: user_id).where("referrals.referrable_id not in (?)", known_keys).order('created_at desc').limit(@limit).offset(@offset)
+      else
+        keys = Referral.unscoped.select(:referrable_id, :created_at, :referrer_id,).where(referrer_id: user_id).order('created_at desc').limit(@limit).offset(@offset)
+      end
+
+      if keys.empty?
+        break
+      end
+
+      keys.each do |ref|
+        media_ids << ref.referrable_id
+        known_keys << ref.referrable_id
+      end
+      
+      media_ids.uniq!
+      known_keys.uniq!
+
+      if @limit == limit
+        @offset = offset + media_ids.length
+      else
+        @offset = @offset + @limit
+      end
     end
+
+    if safe
+      media = Media.unscoped.where("id in (?)", media_ids).nsfw(false)
+    else
+      media = Media.unscoped.where("id in (?)", media_ids)
+    end
+
+    # media.each do |m|
+    #   m.referrals = Array.new
+
+    #   refs = Referral.unscoped.select(:id, :referrer_id, :referrable_id).where(referrer_id: user_id).where(referrable_id: m.id).order('created_at desc')
+
+    #   refs.each do |ref|
+    #     m.referrals << ref.id
+    #   end
+    # end
+
+    media
+
   end
 
   private 
