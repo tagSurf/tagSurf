eb25850af8b224ab9a6e5241289ee33301d7f0d7
VirtualPaul <VirtualPaul@Gmail.com>
2015-03-24 19:11:28 -0700

    rm first time messaging stuff and keep dev servers from sending this email

--- app/mailers/leaderboard_mailer.rb
+++ app/mailers/leaderboard_mailer.rb
@@ -1,5 +1,5 @@
 class LeaderboardMailer < ActionMailer::Base
-  default from: "Paul@tagsurf.co"
+  default from: "tagSurf@tagsurf.co"
 
 	def weekly_leaderboard_mailer(user_id, top_media, winner_id, winner_score)
 		@email = User.find(user_id).email
@@ -9,7 +9,7 @@ class LeaderboardMailer < ActionMailer::Base
 		@winner = User.find(winner_id).username
 		@winner_score = winner_score
     @url = Rails.env.production? ? "http://beta.tagsurf.co" : "http://localhost:3000"
-    mail(to: @email, subject: "Come play a game with me on tagSurf!", from_email: "paul@tagsurf.co", async: "true", from_name: "Paul")
+    mail(to: @email, subject: "And the winner is...", from_email: "tagSurf@tagsurf.co", async: "true", from_name: "tagSurf")
   end
 
 end
