0badf9159bd4674834d943353a1f71896e06e2f7
VirtualPaul <VirtualPaul@Gmail.com>
2015-04-04 17:51:21 -0700

    enable notifications for testing

--- app/workers/send_bump_notification.rb
+++ app/workers/send_bump_notification.rb
@@ -4,9 +4,9 @@ class SendBumpNotification
   sidekiq_options :backtrace => true
 
   def perform(referral_id)
-	if Rails.env.development? 
-		return
-	end
+	# if Rails.env.development? 
+	# 	return
+	# end
 
 	ref = Referral.unscoped.find(referral_id)
 	media = Media.unscoped.find(ref.media_id)
