c617762f28d872c066d3cdca30feb8093a44a256
VirtualPaul <VirtualPaul@Gmail.com>
2015-04-22 17:15:53 -0700

    test if re-route is affecting app link display

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -14,11 +14,11 @@ var castVote = function(card) {
 
 onload = function ()
 {
-	if (isIos() && !isUIWebView() && isAuthorized())
-		window.location = "tagSurf://card/" + document.location.hash.split("#")[1];
-	else if (isIos() && !isUIWebView() && !isAuthorized())
-		window.location = "tagSurf://card/" + document.location.pathname.split("/")[2] + "~" +
-							document.location.pathname.split("/")[3];
+	// if (isIos() && !isUIWebView() && isAuthorized())
+	// 	window.location = "tagSurf://card/" + document.location.hash.split("#")[1];
+	// else if (isIos() && !isUIWebView() && !isAuthorized())
+	// 	window.location = "tagSurf://card/" + document.location.pathname.split("/")[2] + "~" +
+	// 						document.location.pathname.split("/")[3];
 	populateNavbar();
 
 	// defined in util for autocomplete
