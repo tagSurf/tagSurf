5cf440c0b7159c72fe7dda8c086057e6cd11a254
VirtualPaul <VirtualPaul@Gmail.com>
2015-04-27 11:13:05 -0700

    include more info in user buddies list

--- app/assets/javascripts/refer.js
+++ app/assets/javascripts/refer.js
@@ -160,9 +160,15 @@ var refer = {
 				buddyName = document.createElement('div'),
 				checkmark = document.createElement('img'),
 				username = b.users[2] ? b.users[2] : b.users[1].split("@")[0],
+				first_name = b.users[3],
+				last_name = b.users[4],
+				profile_pic = b.users[5],
 				buddy = {
 					id: b.users[0],
 					username: username,
+					first_name: first_name,
+					last_name: last_name,
+					profile_pic: profile_pic,
 					cell: buddyCell, 
 					selected: false
 				};
@@ -171,7 +177,8 @@ var refer = {
 			buddyCell.className = 'buddy-cell';
 			for (var i = 1; i <= username.length; i++)
 				buddyCell.className += " " + username.slice(0, i).toLowerCase();
-			buddyPic.src = "http://assets.tagsurf.co/img/UserAvatar.png";
+			buddyPic.src = profile_pic ? profile_pic : 
+								"http://assets.tagsurf.co/img/UserAvatar.png";
 			buddyPic.className = 'buddy-pic';
 			buddyName.className = 'buddy-name';
 			buddyName.innerHTML += username;  
--- app/models/user.rb
+++ app/models/user.rb
@@ -184,8 +184,8 @@ class User < ActiveRecord::Base
 
     buddy_ids = recent_shares.inject(Hash.new(1)) { |h, e| h[e] += 1 ; h }.to_a.sort_by(&:last).reverse.map {|x,y| x}
 
-    buddies = User.find(buddy_ids).index_by(&:id).values_at(*buddy_ids).map{|u| [u.id,u.email,u.username]}
-    buddies.concat(User.select(:id, :email, :username).order('sign_in_count DESC NULLS LAST').map { |user| [user.id, user.email, user.username] })
+    buddies = User.find(buddy_ids).index_by(&:id).values_at(*buddy_ids).map{|u| [u.id,u.email,u.username, u.first_name, u.last_name, u.profile_pic_link]}
+    buddies.concat(User.select(:id, :email, :username, :first_name, :last_name, :profile_pic_link).order('sign_in_count DESC NULLS LAST').map { |user| [user.id, user.email, user.username, user.first_name, user.last_name, user.profile_pic_link] })
     
     buddies.uniq!
 
