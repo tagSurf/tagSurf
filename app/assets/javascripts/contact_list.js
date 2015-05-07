a3c966b45d4a09839c2beb5c9f0200ef19710000
VirtualPaul <VirtualPaul@Gmail.com>
2015-05-07 13:17:27 -0700

    add contact page specific styles

---
+++ app/assets/javascripts/contact_list.js
@@ -0,0 +1,21 @@
+// populateAnchorLinks = function() {
+// 	var sections = document.getElementsByClassName('section-header'),
+// 			picker = document.getElementById('navigator');
+
+// 	sections.forEach(function(element){
+// 		var letterBox = document.createElement('div'),
+// 				link = document.createElement('a');
+
+// 		letterBox.className = "picker-box";
+// 		letterBox.innerHTML = element.name;
+// 		link.href = "#" + element.name;
+// 		link.appendChild(letterBox);
+// 		picker.appendChild(link);
+// 	})
+// };
+
+// onload = function() {
+// 	populateAnchorLinks();
+// 	alert('Built!');
+// };
+
