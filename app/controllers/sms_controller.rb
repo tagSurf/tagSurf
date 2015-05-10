c611abbef75eca007bf65c9fdf892f97d07be1d4
VirtualPaul <VirtualPaul@Gmail.com>
2015-05-10 16:32:31 -0700

    adjust default message params

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -5,22 +5,22 @@ class SmsController < ApplicationController
     twilio_sid = ENV["TS_TWILIO_SID"]
     twilio_token = ENV["TS_TWILIO_TOKEN"]
     
-    disclaimer = "\n\nReply HELP for help. Reply STOP to unsubscribe. Reply YES to resubscribe."
+    # disclaimer = "\n\nReply HELP for help. Reply STOP to unsubscribe. Reply YES to resubscribe."
 
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
 		@twilio_client.account.sms.messages.create(
       :from => ENV["TS_TWILIO_PHONE_NUMBER"],
       :to => number,
-      :body => message + disclaimer
+      :body => message
     )
   end
 
   def receive
   	message_body = sms_params[:body]
     from_number = sms_params[:from]
- 
-    SMSLogger.log_text_message from_number, message_body
+    #To Do Implement this function 
+    # SMSLogger.log_text_message from_number, message_body
   end
 
   private
