17af882871b1336cd0fd799a1a5e0575e2eb6567
VirtualPaul <VirtualPaul@Gmail.com>
2015-05-11 19:37:15 -0700

    rm 0 from confirmation code since leading 0s cause a problem

--- app/models/confirmation_code.rb
+++ app/models/confirmation_code.rb
@@ -14,7 +14,7 @@ class ConfirmationCode < ActiveRecord::Base
   # Remove the possibility of confusing ints/chars
   # ie... B versus 8, 0 versus O
   def generate_activation_code(size = 4)
-    charset = %w{ 0 1 2 3 4 5 6 7 8 9}
+    charset = %w{ 1 2 3 4 5 6 7 8 9}
     self.code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
   end
 
