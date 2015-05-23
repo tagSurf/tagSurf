705e87a33ef912426a7afe0d76ee845c0146ebeb
VirtualPaul <VirtualPaul@Gmail.com>
2015-05-22 20:48:53 -0700

    rm default close button from reminders

--- app/assets/javascripts/reminder.js
+++ app/assets/javascripts/reminder.js
@@ -80,16 +80,16 @@ var reminder_proto = {
 	},
 	_build: function () {
 		var self = this,
-			container = this.container = document.createElement('div'),
-			closeContainer = document.createElement('div'),
-			close = document.createElement('img');
+			container = this.container = document.createElement('div');
+			// closeContainer = document.createElement('div'),
+			// close = document.createElement('img');
 		container.id = this.type + "-reminder-container";
 		container.className = "reminder-container";
 		container.style.visibility = "hidden";
 		container.style.opacity = 0;
-		close.className = "reminder-close";
-		close.src = "http://assets.tagsurf.co/img/Close.png";
-		closeContainer.appendChild(close);
+		// close.className = "reminder-close";
+		// close.src = "http://assets.tagsurf.co/img/Close.png";
+		// closeContainer.appendChild(close);
 		if(this.node)
 			container.appendChild(this.node);
 		else if (DEBUG)
@@ -100,10 +100,10 @@ var reminder_proto = {
 				return true;
 			}
 		});
-		container.appendChild(closeContainer);
+		// container.appendChild(closeContainer);
 		document.body.appendChild(container);
 		gesture.listen("down", self.container, returnTrue);
-		gesture.listen('down', closeContainer, function() { self.close(); });
+		// gesture.listen('down', closeContainer, function() { self.close(); });
 		gesture.listen("tap", self.container, function() { self.close(); });
 		gesture.listen("swipe", self.container, function() { self.close(); });
 		this.startTimeout(this.delay);
