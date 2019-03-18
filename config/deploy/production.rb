9edf2d2e6170c01dcf8971a32ab9d7ccfd1d88f2
VirtualPaul <VirtualPaul@Gmail.com>
2019-03-17 19:03:58 -0700

    set stage properly

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :rails_env, "production"
+set :stage, :production
+set :rails_env, 'production'
 
 # role-based syntax
 # ==================
